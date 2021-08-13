44fe8bb20e5f983d7d8551530a170477066724bb
설이어미 <tiger6453127@naver.com>
2021-08-14 04:16:17 +0900

    training 페이지 넘김 등 기능 구현,수정, mypage 약간 수정

--- src/components/layout/Header-dark.vue
+++ src/components/layout/Header-dark.vue
@@ -8,6 +8,7 @@
             <router-link class="menu" to="/Signup">Sign up</router-link>
             <img class="nav-menu-division" src="../../assets/nav-menu-division.svg" alt="">
             <button class="menu" @click="openLoginPopup" v-if="_loginState == 0">Login</button>
+            <button class="menu" @click="openLoginPopup" v-if="_loginState == undefined">Login</button>
             <button class="menu" @click="logout" v-if="_loginState == 1">Logout</button>
             <img class="nav-menu-division" v-if="_loginState == 1" src="../../assets/nav-menu-division.svg" alt="">
             <!-- 로그인이 된 경우 마이페이지로 이동 -->
